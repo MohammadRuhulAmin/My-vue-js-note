9314996a821f33238441caba7e95af82a4fb8fbe
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-01-29 01:48:23 +0600

    v-bind  is a directive

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
 new Vue({
     el:"#app",
     data:{
-        title:"Ruhul amin",
-        isItTrue:true,
-        cars:['toyota','lancer','commit'],
-        robot:{
-            name:"robot--1",
-            weigth:"150 kelograam",
-            height:"5.12 inche"
-
-        },
-        myInfo:{
-            name:"Mohammad Ruhul Amin",
-            Email:"alfaBinomialbeta23@gmail.com",
-            contact:"01403288711"
-        },
-        geeting:()=>{
-            return "this is my first function ";
-        }
-    },
-    methods:{
-        my_first_function:function(){
-            return "this function is calling from methods!";
-        },
-        using_own_property:function(){
-            return this.myInfo;
-        }
+        src:"https://www.imagesource.com/wp-content/uploads/2019/06/Rio.jpg",
+        alt:"this is a dog",
     }
-
+        
+     
+    
 });
