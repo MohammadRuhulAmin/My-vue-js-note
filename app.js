7adfe6f991ba15a1a6f6a6c90ff188be75ee2f53
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-01-28 20:32:03 +0600

    object array boolean element

---
+++ app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el:"#app",
+    data:{
+        title:"Ruhul amin",
+        isItTrue:true,
+        cars:['toyota','lancer','commit'],
+        robot:{
+            name:"robot--1",
+            weigth:"150 kelograam",
+            height:"5.12 inche"
+
+        },
+        myInfo:{
+            name:"Mohammad Ruhul Amin",
+            Email:"alfaBinomialbeta23@gmail.com",
+            contact:"01403288711"
+        }
+    }
+
+});
