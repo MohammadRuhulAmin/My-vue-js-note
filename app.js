26b04d29739b174898a68456ab0a504a8bf90abe
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-01-29 12:59:05 +0600

    v-text directives

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ new Vue({
     data:{
         src:"https://www.imagesource.com/wp-content/uploads/2019/06/Rio.jpg",
         alt:"this is a dog",
+        link:"https://www.google.com",
+        youtube_link:"https://www.youtube.com",
+        aboutMyChannel:"this is my first channel to show you about higher math 2nd Paper",
     }
         
      
