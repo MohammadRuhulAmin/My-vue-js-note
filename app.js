f35fa5602f34c08009ba9e4ac4b49dd5a1b12915
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-01-29 13:22:34 +0600

    v-if v-else-if v-else directives conditional rendering

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 new Vue({
     el:"#app",
     data:{
-        src:"https://www.imagesource.com/wp-content/uploads/2019/06/Rio.jpg",
-        alt:"this is a dog",
-        link:"https://www.google.com",
-        youtube_link:"https://www.youtube.com",
-        aboutMyChannel:"this is my first channel to show you about higher math 2nd Paper",
-        myHtml:"<strong>this is a strong text</strong>",
+        userInfo:{
+            name:"mohammad",
+            age:25,
+            allowedAge:26,
+        }
     }
         
      
