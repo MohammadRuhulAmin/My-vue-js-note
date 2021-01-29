f290d82641824b91959915235aed8e35ff9a7c90
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-01-29 23:14:29 +0600

    template implementation

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-new Vue({
-    el:"#app",
+const temp = "  <p>My Name is {{name}}</p>";
+const app1 = new Vue({
+    // el:"#app",
     data:{
-     a:0,
-     b:0,
-     salary:10,
+        name:"Mohammad Ruhul Amin",
     },
-    methods: {
-        
-    },
-    computed:{
-        addToA(){
-            return this.a + this.salary;
-        },
-        addToB(){
-            return this.b + this.salary;
-        }
-    }
-        
-     
+    template:temp,
+      
+    
     
 });
+setTimeout(()=>{
+    app1.$mount('#app')
+},2000);
